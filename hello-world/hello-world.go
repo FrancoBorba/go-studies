dea81f672bf3c4d010cdbe0410c9e0d7b0ed61fa
Franco Borba <franco.borba14@gmail.com>
2025-12-28 09:51:52 -0300

    First code , hello-world

---
+++ hello-world/hello-world.go
@@ -0,0 +1,20 @@
+package main // For the program to be executable, it must be in the "main" package.
+
+/*
+List of imports in go
+*/
+import(   
+	"fmt"
+)
+
+
+/*
+Main function, is the input port of the application, where the code is executed
+*/	
+func main()  {
+	fmt.Println("Hello World")
+}
+
+/*
+To run the application, in the terminal, being in the directory you must run the command "go run filename"
+*/	
