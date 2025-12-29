ee9800bb2ada466022c0c5d771ed6df6bfdf57b4
Franco Borba <franco.borba14@gmail.com>
2025-12-29 09:28:53 -0300

    Learn how to install external packages

--- 1-Packages/main.go
+++ 1-Packages/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"modulo/Assistant"
+
+	"github.com/badoux/checkmail"
 )
 
 func main () {
 
 	fmt.Println("Writing from main file")
 	assistant.Writing()
+
+	erro := checkmail.ValidateFormat("franco.borba	@gmail")
+	fmt.Println(erro)
 }
 
