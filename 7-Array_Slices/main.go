694bfcfc2a62aaac78d1e486e9f1a0409a74545a
Franco Borba <franco.borba14@gmail.com>
2025-12-29 21:32:02 -0300

    Arrays and Slices

---
+++ 7-Array_Slices/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main()  {
+	
+	// Create a array of 5 position 
+	var array[5] int
+	fmt.Println(array)
+
+	array[0] = 3
+	array[1] = 6
+
+	fmt.Println(array)
+
+	// Fill in the first 3 positions and leave the others at default value
+	array1 := [5]string {"Posision 1" , "Positions 2 ", "Test "}
+
+	fmt.Println(array1)
+
+	// -------------- SLICE --------------------
+
+	slice := []int{1,34,556,33,2,9}
+	fmt.Println(slice)
+	
+	fmt.Println(reflect.TypeOf(slice) )
+	fmt.Println(reflect.TypeOf(array) )
+
+	slice = append(slice, 6)
+
+	fmt.Println(slice)
+
+	/*
+	
+	It points to array 2, it's like a pointer. 
+	The first parameter is inclusive, that is, it starts from it. 
+	The second parameter is exclusive, that is, it goes to the previous one in this case, it goes from 0 to 1 
+	*/
+	slice2 := array[0:2]
+	fmt.Println(slice2)
+
+	slice2 = append(slice2, 9 , 12)
+
+	fmt.Println(slice2)
+
+	array[0] = 1 // will change in the slice
+	fmt.Println(slice2)
+
+
+
+}
